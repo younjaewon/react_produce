424f68cfbf6345b7402461fd76ee9e01bafb48aa
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-02-25 09:05:48 +0900

    react router dom navigation

--- src/App.js
+++ src/App.js
@@ -4,17 +4,32 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import MaterialOutManage from "./components/MaterialOutManage";
 import RequirementManage from "./components/RequirementManage";
 import RequirementBom from "./components/RequirementBom";
-import { Route, Routes } from "react-router-dom";
-import { useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 function App() {
-  const [viewContent, setViewContent] = useState("material");
+  const navigate = useNavigate();
 
   const changeView = (e) => {
-    setViewContent(e.target.name);
+    navigate(`/${e.target.name}`);
   };
   return (
     <>
+      <div>
+        <button
+          style={{ border: "none", margin: "5px" }}
+          name=""
+          onClick={changeView}
+        >
+          자재준비 관리
+        </button>
+        <button
+          style={{ border: "none", margin: "5px" }}
+          name="require"
+          onClick={changeView}
+        >
+          소요량 관리
+        </button>
+      </div>
       <Routes>
         <Route exact path="/" element={<MaterialOutManage />} />
         <Route exact path="/require" element={<RequirementManage />} />
